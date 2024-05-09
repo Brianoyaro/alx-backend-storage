ec5f036700400df60fbf91e3b289f975f7417452
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-09 11:43:40 +0300

    tasks 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -4,11 +4,9 @@ CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS DECIMAL(10, 2) DETERMINISTIC
 BEGIN
 	DECLARE result DECIMAL(10, 2);
-	IF b = 0 THEN
-		SET result = 0.00
-	ELSE
-		SET result = a / b;
-	END
+	IF b = 0 THEN SET result = 0.00;
+	ELSE SET result = a / b;
+	END IF;
 	RETURN result;
 END//
 DELIMITER ;
