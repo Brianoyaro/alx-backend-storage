0dbbf500b5c2203090f6e056a5cc0c7e2d5e6c00
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-15 13:35:36 +0300

    initial commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,19 @@
 import redis
 import uuid
 from typing import Callable, Union, Any
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        if self._redis.get(key) is None:
+            self._redis.set(key, 1)
+        else:
+            self._redis.incr(key, 1)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache():
     """redis class"""
@@ -11,12 +23,13 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, int, float, bytes]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable) -> Union[int, str, None, bytes]:
+    def get(self, key: str, fn: Callable[..., Any] = None) -> Union[int, str, None, bytes]:
         value = self._redis.get(key)
         if value is not None and fn is not None:
             return fn(value)
