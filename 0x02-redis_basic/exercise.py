5a927a76d18010baefe6370acb5d74b1e2e986bf
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-15 13:05:05 +0300

    initial commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """sample python redis implementation module"""
 import redis
 import uuid
+from typing import Callable, Union, Any
 
 
 class Cache():
@@ -9,7 +10,20 @@ class Cache():
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-    def store(self, data):
+
+    def store(self, data: Union[str, int, float, bytes]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable) -> Union[int, str, None, bytes]:
+        value = self._redis.get(key)
+        if value is not None and fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, val: str) -> Union[str, None]:
+        return str(val)
+
+    def get_int(self, val: str) -> Union[int, None]:
+        return int(val)
